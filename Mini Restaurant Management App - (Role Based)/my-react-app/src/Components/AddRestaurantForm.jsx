2d4246fdfe2f8b980a433c2c7fbf5b80e40fa073
Revatee Ghatage <ghatagerevati3@gmail.com>
2026-01-03 20:27:34 +0530

    Comitted changes

--- Mini Restaurant Management App - (Role Based)/my-react-app/src/Components/AddRestaurantForm.jsx
+++ Mini Restaurant Management App - (Role Based)/my-react-app/src/Components/AddRestaurantForm.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddRestaurantForm = () => {
+
+    const[name,setName] = useState("")
+    const [address, setAddress] = useState("")
+    const [type, setType] = useState("Rajasthani")
+    const [parking, setParking] = useState(true)
+
+    const handleStuff= () =>{
+        if(!name || !address){
+            alert("Fill all fields");
+            return
+        }
+
+        const data = getRestaurants();
+
+        const newItem = {
+                restaurantName: name,
+                address: address,
+                type: type,
+                parkingLot: true,
+                image: "https://coding-platform.s3.amazonaws.com/dev/lms/tickets/7524df6e-46fa-4506-8766-eca8da47c2f1/2izhqnTaNLdenHYF.jpeg"
+            }
+
+
+
+        }
+    }
+    
   return (
     <div>
-      
+      <h3>Add Restaurants</h3>
+
     </div>
   )
-}
+
 
 export default AddRestaurantForm
--- Mini Restaurant Management App - (Role Based)/my-react-app/src/Components/RestaurantCard.jsx
+++ Mini Restaurant Management App - (Role Based)/my-react-app/src/Components/RestaurantCard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const RestaurantCard = () => {
+const RestaurantCard = ({item,deleteItem}) => {
+    const navigate = useNavigate();
+    
   return (
     <div>
-      
+      <div className='card'>
+        <img src = {item.image} />
+        <h2>{item.address}</h2>
+        <h2>{item.type}</h2>
+        <h2>{item.parkingLot}</h2>
+
+      </div>
     </div>
   )
 }
