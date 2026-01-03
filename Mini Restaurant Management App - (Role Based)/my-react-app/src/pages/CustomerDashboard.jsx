f63300f417c47e53e0f5500fe0e26e9db0eba296
Revatee Ghatage <ghatagerevati3@gmail.com>
2026-01-03 20:41:04 +0530

    Changes Applied

--- Mini Restaurant Management App - (Role Based)/my-react-app/src/pages/CustomerDashboard.jsx
+++ Mini Restaurant Management App - (Role Based)/my-react-app/src/pages/CustomerDashboard.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const CustomerDashboard = () => {
+
+    const[data,setData] =useState([])
+        const [search, setSearch] = useState("")
+        const [filter, setFilter] = useState("")
+        
   return (
     <div>
       
